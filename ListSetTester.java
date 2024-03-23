7edbb03e7062091f4b04abfbc773d6217acdde91
Neev Gupta <87718918+guptaneev@users.noreply.github.com>
2024-03-23 12:55:20 -0500

    Create ListSetTester.java

---
+++ ListSetTester.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class ListSetTester
+{
+	public static void main(String[] args)
+	{
+		List<String> a = new ListSet<String>();
+
+		System.out.println("ListSet Tester:");		// ListSet Tester:
+		System.out.println("---------------");		// ---------------
+		System.out.println("Empty list: " + a);		// Empty list: []
+		
+		// TODO: Insert code to thoroughly test each of your methods...
+		
+		
+		
+		
+		
+		System.out.println("---------------");		// ---------------
+		System.out.println("Done.");
+	}
+}
